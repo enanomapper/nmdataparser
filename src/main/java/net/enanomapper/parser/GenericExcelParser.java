25dfa14a893638fd74cabfa5bcad91c74c700145
ntk73 <nick@uni-plovdiv.net>
2014-10-18 15:44:10 +0300

    Added class GenericExcelParser

---
+++ src/main/java/net/enanomapper/parser/GenericExcelParser.java
@@ -0,0 +1,17 @@
+package net.enanomapper.parser;
+
+import java.util.ArrayList;
+
+
+/**
+ * 
+ * @author nick
+ *
+ */
+public class GenericExcelParser 
+{
+	private ArrayList<String> parseErrors = new ArrayList<String> ();
+	
+	//TODO
+	
+}
