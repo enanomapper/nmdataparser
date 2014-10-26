3525fce64bda2564bab4890934246546037b4bba
ntk73 <nick@uni-plovdiv.net>
2014-10-26 19:04:52 +0200

    Added enum IterationAcess. Some test code for json.

--- src/main/java/net/enanomapper/parser/ExcelParserConfigurator.java
+++ src/main/java/net/enanomapper/parser/ExcelParserConfigurator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 
@@ -14,19 +15,40 @@ import org.codehaus.jackson.node.ObjectNode;
  */
 public class ExcelParserConfigurator 
 {
+	public enum IterationAccess {
+		ROW_SINGLE, ROW_MULTI_FIXED, ROWS_MULTI_DYNAMIC, UNDEFINED;
+		
+		public static IterationAccess fromString(String s)
+		{	 
+			try
+			{
+				IterationAccess access = IterationAccess.valueOf(s) ;
+				return (access);
+			}
+			catch (Exception e)
+			{
+				return IterationAccess.UNDEFINED;
+			}
+		}
+	}
+	
+	
+	public ArrayList<String> configErrors = new ArrayList<String> ();
+	
 	public int configurationType = 1;
 	public int startRow = 1;
 	public int headerRow = 1;
-	public ArrayList<String> configErrors = new ArrayList<String> ();
+	public String templateName = ""; 
+	
 	
 	public static ExcelParserConfigurator loadFromJSON(String jsonConfig) throws Exception
 	{
 		FileInputStream fin = new FileInputStream(jsonConfig); 
 		ObjectMapper mapper = new ObjectMapper();
-		ObjectNode rootNode = null;
+		JsonNode root = null;
 		
 		try {
-			rootNode = (ObjectNode)mapper.readTree(fin);
+			root = mapper.readTree(fin);
 		} catch (Exception x) {
 			throw x;
 		} finally {
@@ -39,6 +61,17 @@ public class ExcelParserConfigurator
 		return epConfig;
 	}
 	
+	public String toJSONString()
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append("{\n");
+		sb.append("   \"template_name\" : \"" + templateName + "\",\n" );
+		
+		
+		
+		sb.append("}\n");
+		return sb.toString();
+	}
 	
 	public String getAllErrorsAsString()
 	{
--- src/test/java/net/enanomapper/parser/test/NMParserTestUtils.java
+++ src/test/java/net/enanomapper/parser/test/NMParserTestUtils.java
@@ -1,10 +1,35 @@
 package net.enanomapper.parser.test;
 
+import net.enanomapper.parser.ExcelParserConfigurator;
+import net.enanomapper.parser.ExcelParserConfigurator.IterationAccess;
+
 public class NMParserTestUtils {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args)  throws Exception
+	{
+		//testExcelParserConfiguration("/Users/nick/Projects/eNanoMapper/config01.json");
+		
+		System.out.println(IterationAccess.fromString("ROW_SINGLE"));
+		
 
+	}
+	
+	public static void testExcelParserConfiguration(String jsonFile) throws Exception
+	{
+		ExcelParserConfigurator parserConfig = ExcelParserConfigurator.loadFromJSON(jsonFile);
+		
+		if (parserConfig.configErrors.size() > 0)
+		{	
+			System.out.println("GenericExcelParser configuration errors:\n" + parserConfig.getAllErrorsAsString());
+			return;
+		}	
+		
+		System.out.println("ExcelParserConfigurator " + jsonFile);
+		System.out.println(parserConfig.toJSONString());
 	}
 
 }
