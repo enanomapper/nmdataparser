66b56d23b0f99e04bafb07c566262ae685a50cd0
ntk73 <nick@uni-plovdiv.net>
2014-12-07 07:25:26 +0200

    Added excel locations for the application protocol and toJSON... utility finction

--- src/main/java/net/enanomapper/parser/ProtocolApplicationDataLocation.java
+++ src/main/java/net/enanomapper/parser/ProtocolApplicationDataLocation.java
@@ -1,6 +1,20 @@
 package net.enanomapper.parser;
 
+import java.util.HashMap;
+
 public class ProtocolApplicationDataLocation 
 {
-
+	public ExcelDataLocation locUUID = null;
+	public HashMap<String, ExcelDataLocation> protocolLocations = new HashMap<String, ExcelDataLocation>();
+	public HashMap<String, ExcelDataLocation> parametersLocations = new HashMap<String, ExcelDataLocation>();
+	
+	
+	public String toJSONKeyWord(String offset)
+	{	
+		StringBuffer sb = new StringBuffer();
+		sb.append(offset + "{\n");
+		
+		return sb.toString();
+	}	
+	
 }
