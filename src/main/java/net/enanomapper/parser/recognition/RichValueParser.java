f4122475aa90bb5feb7e69a374ba95306e2d2845
ntk73 <nick@uni-plovdiv.net>
2015-01-16 12:50:15 +0200

    Implemented functionality for parsing rich values

--- src/main/java/net/enanomapper/parser/recognition/RichValueParser.java
+++ src/main/java/net/enanomapper/parser/recognition/RichValueParser.java
@@ -1,22 +1,85 @@
 package net.enanomapper.parser.recognition;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class RichValueParser 
 {
+	//Setup variables and flags
+	private String tokenSplitter = ";";
+	
+	//Work variables
+	protected String rvString = null;
+	protected RichValue rvalue = null;
+	protected ArrayList<String> errors = new ArrayList<String>();
+	
+	protected int curTokCh;
+	protected int nTokChars;
+	protected String curToken = null;
+	protected int curTokenNum = 0;
+	
+	
+	public String getTokenSplitter() {
+		return tokenSplitter;
+	}
+
+	public void setTokenSplitter(String tokenSplitter) {
+		this.tokenSplitter = tokenSplitter;
+	}
+	
 	public RichValue parse(String rvStr)
 	{
-		RichValue rvalue = new RichValue();
-		int nChar = rvStr.length();
-		int pos = 0;
-		while (pos < nChar)
+		if (rvStr == null)
 		{
-			char ch = rvStr.charAt(pos);
-			
-			
+			errors.add("Empty input string!");
+			return null;
 		}
 		
+		rvString = rvStr;
+		
+		String tokens[] = rvString.split(tokenSplitter);
+		
+		curTokenNum = 1;
+		rvalue = parseToken(tokens[0]);
+		if (!errors.isEmpty())
+			return null;
+		
+		for (int i = 1; i < tokens.length; i++)
+		{
+			curTokenNum = i+1;
+			RichValue rv = parseToken(tokens[i]);
+			if (errors.isEmpty())
+				rvalue.additionalValues.add(rv);
+			else
+			{	
+				return null;
+			}	
+		}
 		
 		return rvalue;
 	}
+	
+	public List<RichValue> parseAsList(String rvStr)
+	{
+		if (rvStr == null)
+		{
+			errors.add("Empty input string!");
+			return null;
+		}
+		
+		//TODO
+		
+		return null;
+	}
+	
+	
+	RichValue parseToken(String token)
+	{
+		//TODO
+		return null;
+	}
+
+	
 }
