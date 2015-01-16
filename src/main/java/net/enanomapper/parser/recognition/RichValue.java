ba75775e4d90f7018a2a14a5514f6b72e2c62dcc
ntk73 <nick@uni-plovdiv.net>
2015-01-16 12:52:23 +0200

    Added new class RichValue

---
+++ src/main/java/net/enanomapper/parser/recognition/RichValue.java
@@ -0,0 +1,15 @@
+package net.enanomapper.parser.recognition;
+
+import java.util.List;
+
+public class RichValue 
+{	
+	public Double loValue = null;
+	public String loQualifier = null;
+	public Double upValue = null;
+	public String upQualifier = null;
+	public String unit = null;
+	public String errorMsg = null;
+	
+	public List<RichValue> additionalValues = null;
+}
