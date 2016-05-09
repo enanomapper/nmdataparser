aed8907a8fa0cfd6c70b8ee2615ee7269949bb76
ngn <jeliazkova.nina@gmail.com>
2016-05-09 14:22:33 +0300

    command line convertor excel/isa(and other formats)

---
+++ src/main/java/net/enanomapper/parser/app/DataConvertor.java
@@ -0,0 +1,422 @@
+package net.enanomapper.parser.app;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+
+import net.enanomapper.parser.GenericExcelParser;
+import net.enanomapper.parser.InvalidCommand;
+import net.idea.loom.nm.nanowiki.NanoWikiRDFReader;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
+import org.apache.log4j.PropertyConfigurator;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import ambit2.base.data.Property;
+import ambit2.base.data.SubstanceRecord;
+import ambit2.base.data.study.StructureRecordValidator;
+import ambit2.base.data.substance.SubstanceEndpointsBundle;
+import ambit2.base.interfaces.IStructureRecord;
+import ambit2.base.relation.composition.CompositionRelation;
+import ambit2.core.io.IRawReader;
+import ambit2.export.isa.v1_0.ISAJsonExporter1_0;
+
+public class DataConvertor {
+
+	protected static Logger logger_cli = Logger.getLogger(DataConvertor.class
+			.getName());
+	static final String loggingProperties = "net/enanomapper/logging.properties";
+	static final String log4jProperties = "net/enanomapper/log4j.properties";
+	static {
+		logger_cli = Logger.getLogger("dataconvertor", "net.enanomapper.msg");
+		Locale.setDefault(Locale.ENGLISH);
+		String dOption = System.getProperty("java.util.logging.config.file");
+		if (dOption == null || "".equals(dOption)) {
+			InputStream in = null;
+			try {
+				in = DataConvertor.class.getClassLoader().getResourceAsStream(
+						loggingProperties);
+				LogManager.getLogManager().readConfiguration(in);
+
+			} catch (Exception x) {
+				logger_cli.log(Level.WARNING, x.getMessage());
+			} finally {
+				try {
+					in.close();
+				} catch (Exception x) {
+				}
+			}
+		}
+		// now log4j for those who use it
+		InputStream in = null;
+		try {
+			in = DataConvertor.class.getClassLoader().getResourceAsStream(
+					log4jProperties);
+			PropertyConfigurator.configure(in);
+
+		} catch (Exception x) {
+			logger_cli.log(Level.WARNING, x.getMessage());
+		} finally {
+			try {
+				in.close();
+			} catch (Exception x) {
+			}
+		}
+
+	}
+	protected boolean gzipped = false;
+	protected File jsonConfig;
+	protected File inputFile;
+	protected File outputFile;
+
+	public File getInputFile() {
+		return inputFile;
+	}
+
+	public void setInputFile(File inputFile) {
+		this.inputFile = inputFile;
+		if (inputFile.toString().toLowerCase().endsWith(".gz"))
+			gzipped = true;
+		else
+			gzipped = false;
+	}
+
+	protected IRawReader<IStructureRecord> createParser(InputStream stream,
+			String extension) throws Exception {
+
+		InputStream in = null;
+		if (gzipped)
+			in = new GZIPInputStream(stream);
+		else
+			in = stream;
+
+		boolean xlsx = extension == null ? false : extension.toLowerCase()
+				.endsWith("xlsx");
+		boolean xls = extension == null ? false : extension.toLowerCase()
+				.endsWith("xls");
+		boolean rdf = extension == null ? false : extension.toLowerCase()
+				.endsWith("rdf");
+		if (xlsx || xls) {
+			return new GenericExcelParser(in, jsonConfig, xlsx);
+		} else if (rdf)
+			return new NanoWikiRDFReader(new InputStreamReader(in));
+		else
+			throw new Exception("Unsupported format");
+	}
+
+	/*
+	 * protected IRawReader<IStructureRecord> createParser(InputStream stream,
+	 * boolean xlsx) throws Exception { _parsertype mode = getParserType();
+	 * InputStream in = null; if (gzipped) in = new GZIPInputStream(stream);
+	 * else in = stream; if (mode == null) throw new
+	 * Exception("Unsupported parser type " + mode);
+	 * 
+	 * switch (mode) { case nanowiki: return new NanoWikiRDFReader(new
+	 * InputStreamReader(in)); case modnanotox: return new MNTParser(in,
+	 * jsonConfig, true); default: return super.createParser(in, xlsx); } }
+	 */
+
+	protected static File getJSONConfig(CommandLine line)
+			throws FileNotFoundException {
+		if (line.hasOption('j')) {
+			File file = new File(line.getOptionValue('j'));
+			if (!file.exists())
+				throw new FileNotFoundException(file.getName());
+			else
+				return file;
+		} else
+			return null;
+	}
+
+	protected static File getInput(CommandLine line)
+			throws FileNotFoundException {
+		if (line.hasOption('i')) {
+			File file = new File(line.getOptionValue('i'));
+			if (!file.exists())
+				throw new FileNotFoundException(file.getName());
+			else
+				return file;
+		} else
+			return null;
+	}
+
+	protected static File getOutput(CommandLine line) {
+		if (line.hasOption('o')) {
+			return new File(line.getOptionValue('o'));
+		} else
+			return null;
+	}
+
+	public boolean parse(String[] args) throws Exception {
+		final Options options = createOptions();
+		CommandLineParser parser = new PosixParser();
+		try {
+			CommandLine line = parser.parse(options, args, false);
+
+			setInputFile(getInput(line));
+
+			if (inputFile == null && !inputFile.exists())
+				throw new Exception("Missing input file");
+			jsonConfig = getJSONConfig(line);
+
+			String extension = inputFile.getName().toLowerCase();
+			boolean xlsx = extension == null ? false : extension
+					.endsWith("xlsx");
+			boolean xls = extension == null ? false : extension.endsWith("xls");
+
+			if (xlsx || xls)
+				if (jsonConfig == null)
+					throw new Exception(
+							"Missing JSON config file, mandatory for importing XLSX!");
+
+			outputFile = getOutput(line);
+
+			return true;
+		} catch (Exception x) {
+			printHelp(options, x.getMessage());
+			throw x;
+		} finally {
+
+		}
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	protected int convertFile() throws Exception {
+		if (inputFile.isDirectory()) {
+			logger_cli.log(Level.INFO, "MSG_IMPORT", new Object[] { "folder",
+					inputFile.getAbsolutePath() });
+			File[] allFiles = inputFile.listFiles();
+			long started = System.currentTimeMillis();
+			int allrecords = 0;
+			for (int i = 0; i < allFiles.length; i++) {
+				File file = allFiles[i];
+				setInputFile(file);
+				try {
+					allrecords += convertFile();
+				} catch (Exception x) {
+					logger_cli.log(Level.INFO, "MSG_ERR",
+							new Object[] { x.getMessage() });
+				} finally {
+					long now = System.currentTimeMillis();
+					logger_cli.log(Level.INFO, "MSG_INFO_RECORDS",
+							new Object[] {
+									(i + 1),
+									(double) (now - started)
+											/ ((double) (i + 1)),
+									allrecords,
+									(double) (now - started)
+											/ ((double) allrecords) });
+				}
+			}
+			return allrecords;
+		} else {
+			String ext = inputFile.toString().toLowerCase();
+			return convertFile(ext.endsWith(".xlsx"));
+
+		}
+	}
+
+	public int write(IRawReader<IStructureRecord> reader,
+			StructureRecordValidator validator) throws Exception {
+		SubstanceEndpointsBundle endpointBundle = null;
+		endpointBundle = new SubstanceEndpointsBundle();
+		endpointBundle.setDescription(inputFile.getName());
+		endpointBundle.setTitle(inputFile.getName());
+
+		ISAJsonExporter1_0 exporter;
+		try {
+			exporter = new ISAJsonExporter1_0(null, null);
+			exporter.init(endpointBundle);
+		} catch (Exception x) {
+			logger_cli.log(Level.SEVERE, x.getMessage());
+			throw x;
+		}
+
+		int records = 0;
+		try {
+			while (reader.hasNext()) {
+				Object record = reader.next();
+				if (record == null)
+					continue;
+				try {
+					validator.process((IStructureRecord) record);
+					exporter.process((SubstanceRecord) record);
+				} catch (Exception x) {
+					logger_cli.log(Level.FINE, x.getMessage());
+				}
+				records++;
+			}
+			try {
+				ObjectMapper mapper = new ObjectMapper();
+				mapper.setSerializationInclusion(Inclusion.NON_EMPTY);
+				mapper.writerWithDefaultPrettyPrinter().writeValue(outputFile,
+						exporter.getOutput());
+			} catch (Exception x) {
+				logger_cli.log(Level.WARNING, x.getMessage());
+			} finally {
+				try {
+					exporter.close();
+				} catch (Exception x) {
+				}
+				exporter = null;
+			}
+		} catch (Exception x) {
+			logger_cli.log(Level.WARNING, x.getMessage(), x);
+		} finally {
+			if (exporter != null)
+				exporter.close();
+			logger_cli
+					.log(Level.INFO, "MSG_IMPORTED", new Object[] { records });
+		}
+		return records;
+	}
+
+	protected int convertFile(boolean xlsx) throws Exception {
+		IRawReader<IStructureRecord> parser = null;
+		Connection c = null;
+		try {
+			FileInputStream fin = new FileInputStream(inputFile);
+
+			parser = createParser(fin, inputFile.getName());
+			logger_cli.log(Level.INFO, "MSG_IMPORT", new Object[] {
+					parser.getClass().getName(), inputFile.getAbsolutePath() });
+
+			StructureRecordValidator validator = new StructureRecordValidator(
+					inputFile.getName(), true) {
+				@Override
+				public IStructureRecord validate(SubstanceRecord record)
+						throws Exception {
+					if (record.getRelatedStructures() != null
+							&& !record.getRelatedStructures().isEmpty()) {
+
+						for (int i = record.getRelatedStructures().size() - 1; i >= 0; i--) {
+							CompositionRelation rel = record
+									.getRelatedStructures().get(i);
+							int props = 0;
+							for (Property p : rel.getSecondStructure()
+									.getRecordProperties()) {
+								Object val = rel.getSecondStructure()
+										.getRecordProperty(p);
+								if (val != null && !"".equals(val.toString()))
+									props++;
+							}
+							if ((rel.getContent() == null || "".equals(rel
+									.getContent())) && (props == 0))
+								record.getRelatedStructures().remove(i);
+
+						}
+
+					}
+					return super.validate(record);
+				}
+			};
+
+			return write(parser, validator);
+		} catch (Exception x) {
+			throw x;
+		} finally {
+			if (parser != null)
+				parser.close();
+			try {
+				c.close();
+			} catch (Exception x) {
+			}
+		}
+	}
+
+	protected Options createOptions() {
+		Options options = new Options();
+		Option input = OptionBuilder.hasArg().withLongOpt("input")
+				.withArgName("file").withDescription("Input file or folder")
+				.create("i");
+
+		Option jsonConfig = OptionBuilder.hasArg().withLongOpt("json")
+				.withArgName("file").withDescription("JSON config file")
+				.create("j");
+
+		Option output = OptionBuilder.hasArg().withLongOpt("output")
+				.withArgName("file").withDescription("Output file").create("o");
+
+		/*
+		 * Option gzip = OptionBuilder.hasArg().withLongOpt("gzipped")
+		 * .withDescription("Gzipped file").create("z");
+		 */
+		Option help = OptionBuilder.withLongOpt("help")
+				.withDescription("This help").create("h");
+
+		options.addOption(input);
+		options.addOption(jsonConfig);
+		options.addOption(output);
+
+		options.addOption(help);
+
+		return options;
+	}
+
+	public static void main(String[] args) {
+		logger_cli.log(Level.INFO, "MSG_INFO_VERSION");
+		long now = System.currentTimeMillis();
+		int code = 0;
+		try {
+			DataConvertor object = new DataConvertor();
+			if (object.parse(args)) {
+				object.convertFile();
+			} else
+				code = -1;
+
+		} catch (ConnectException x) {
+			logger_cli.log(Level.SEVERE, "MSG_CONNECTION_REFUSED",
+					new Object[] { x.getMessage() });
+			Runtime.getRuntime().runFinalization();
+			code = -1;
+
+		} catch (SQLException x) {
+			logger_cli.log(Level.SEVERE, "MSG_ERR_SQL",
+					new Object[] { x.getMessage() });
+			code = -1;
+		} catch (InvalidCommand x) {
+			logger_cli.log(Level.SEVERE, "MSG_INVALIDCOMMAND",
+					new Object[] { x.getMessage() });
+			code = -1;
+		} catch (Exception x) {
+			logger_cli.log(Level.SEVERE, "MSG_ERR", new Object[] { x });
+			code = -1;
+		} finally {
+			if (code >= 0)
+				logger_cli.log(Level.INFO, "MSG_INFO_COMPLETED",
+						(System.currentTimeMillis() - now));
+		}
+	}
+
+	protected static void printHelp(Options options, String message) {
+		if (message != null)
+			logger_cli.log(Level.WARNING, message);
+
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("dataconvertor-{version}", options);
+		Runtime.getRuntime().runFinalization();
+		Runtime.getRuntime().exit(0);
+	}
+
+}
