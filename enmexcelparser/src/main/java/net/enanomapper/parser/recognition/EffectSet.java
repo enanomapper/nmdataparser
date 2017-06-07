1015d0a7eb414906a4955b7c9b37fa18100e2af2
ntk73 <nick@uni-plovdiv.net>
2017-06-07 23:56:44 +0300

    Implemented function toString(). Added helper function effectToString()

--- enmexcelparser/src/main/java/net/enanomapper/parser/recognition/EffectSet.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/recognition/EffectSet.java
@@ -2,12 +2,28 @@ package net.enanomapper.parser.recognition;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class EffectSet 
 {
 	public Map<String, Object> effects = new HashMap<String, Object>();
 	
 	public String toString(EffectSetPattern pattern)
+	{
+		StringBuffer sb = new StringBuffer();
+		Set<String> endpoints = effects.keySet();
+		int n = 0;
+		for (String ep : endpoints)
+		{
+			if (n > 0)
+				sb.append(" " + pattern.effectSeparator);
+			Object value = effects.get(ep);
+			sb.append(effectToString(ep, value, pattern));
+		}
+		return sb.toString();
+	}
+	
+	public String effectToString(String endpoint, Object value, EffectSetPattern pattern)
 	{
 		StringBuffer sb = new StringBuffer();
 		//TODO
