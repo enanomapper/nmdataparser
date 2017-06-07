ff5487cbbb526738fb318e1afeb28ca160c46076
ntk73 <nick@uni-plovdiv.net>
2017-06-07 22:44:30 +0300

    Added new classes for handling effects set from a single cell

---
+++ enmexcelparser/src/main/java/net/enanomapper/parser/recognition/EffectSet.java
@@ -0,0 +1,16 @@
+package net.enanomapper.parser.recognition;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EffectSet 
+{
+	public Map<String, Object> effects = new HashMap<String, Object>();
+	
+	public String toString(EffectSetPattern pattern)
+	{
+		StringBuffer sb = new StringBuffer();
+		//TODO
+		return sb.toString();
+	}
+}
---
+++ enmexcelparser/src/main/java/net/enanomapper/parser/recognition/EffectSetParser.java
@@ -0,0 +1,6 @@
+package net.enanomapper.parser.recognition;
+
+public class EffectSetParser 
+{
+
+}
