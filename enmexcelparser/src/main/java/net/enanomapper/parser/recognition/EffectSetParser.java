46fd074e4ae37fbadde53ec47b543262fa451d0a
ntk73 <nick@uni-plovdiv.net>
2017-06-10 16:14:56 +0300

    Implemented initial version of EffectSet.parser() and error helpers

--- enmexcelparser/src/main/java/net/enanomapper/parser/recognition/EffectSetParser.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/recognition/EffectSetParser.java
@@ -1,6 +1,40 @@
 package net.enanomapper.parser.recognition;
 
+import java.util.ArrayList;
+
 public class EffectSetParser 
 {
+	// Work variables
+	protected ArrayList<String> errors = new ArrayList<String>();
+	
+	
+	public ArrayList<String> getErrors() {
+		return errors;
+	}
+	
+	public String getAllErrorsAsString() {
+		if (errors.isEmpty())
+			return null;
 
+		StringBuffer sb = new StringBuffer();
+		for (String err : errors)
+			sb.append(err + "\n");
+		return sb.toString();
+	}
+	
+	public EffectSet parse(String esStr)
+	{
+		errors.clear();
+
+		if (esStr == null) {
+			errors.add("Empty string!");
+			return null;
+		}
+		
+		EffectSet es = new EffectSet();
+		
+		
+		//TODO
+		return es;
+	}
 }
