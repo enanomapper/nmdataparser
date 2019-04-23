9b401fbeb53bd372b7c85454e4049de818dff6c7
Nikolay Kochev <nick@uni-plovdiv.net>
2019-04-23 11:23:01 +0300

    Added function extractTokenizer()

--- enmexcelparser/src/main/java/net/enanomapper/parser/recognition/Tokenize.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/recognition/Tokenize.java
@@ -2,6 +2,11 @@ package net.enanomapper.parser.recognition;
 
 import java.util.List;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import net.enanomapper.parser.ExcelParserConfigurator;
+import net.enanomapper.parser.json.JsonUtilities;
+
 
 public class Tokenize 
 {
@@ -9,8 +14,29 @@ public class Tokenize
 		REGIONS, SPLIT, UNDEFINED  
 	}
 	
-	public Mode mode = Mode.REGIONS;	
+	public String name = null;
+	public Mode mode = null;	
 	public String splitter = null;
 	public List<TokenRegion> regions = null;
 	
+	
+	
+	public static Tokenize extractTokenizer(JsonNode node, ExcelParserConfigurator conf, int valueGroupNum) 
+	{
+		Tokenize tok = new Tokenize();
+
+		JsonUtilities jsonUtils = new JsonUtilities();
+		String keyword;
+		
+		//NAME
+		if (!node.path("NAME").isMissingNode()) {
+			keyword = jsonUtils.extractStringKeyword(node, "NAME", false);
+			if (keyword == null)
+				conf.addError(jsonUtils.getError());
+			else 
+				tok.name = keyword;
+		}
+
+		return tok;
+	}
 }
