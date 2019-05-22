28a236c05d327cea58fa3a90f1c412654420754f
ntk73 <nick@uni-plovdiv.net>
2019-05-22 21:11:38 +0300

    Implemented function toJSONKeyWord()

--- enmexcelparser/src/main/java/net/enanomapper/parser/recognition/TokenRegion.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/recognition/TokenRegion.java
@@ -9,7 +9,7 @@ public class TokenRegion
 {
 	public static enum Type {
 		FIXED_NUM_OF_CHARACTERS, SUBSTRING, 
-		NUMBER_SEQUENCE, ALPH_SEQUENCE, ALPHANUMERIC_SEQUENCE, 
+		NUMBER_SEQUENCE, ALPHA_SEQUENCE, ALPHANUMERIC_SEQUENCE, 
 		UNDEFINED;  
 		
 		public static Type fromString(String s) {
@@ -23,9 +23,15 @@ public class TokenRegion
 	}
 	
 	public Type regionType = null;
-	public int numChars = -1;
+	
+	public Integer numOfChars = -1;
+	public boolean FlagNumOfChars = false;
+	
 	public int beginIndex = -1;
+	public boolean FlagBeginIndex = false;
+	
 	public int endIndex = -1;
+	public boolean FlagEndIndex = false;
 	
 	public static TokenRegion extractTokenRegion(JsonNode node, ExcelParserConfigurator conf, 
 			JsonUtilities jsonUtils) 
@@ -36,5 +42,54 @@ public class TokenRegion
 		
 		return reg;
 	}
+	
+	public String toJSONKeyWord(String offset) {
+		int nFields = 0;
+		StringBuffer sb = new StringBuffer();
+
+		sb.append(offset + "{\n");
+
+		if (regionType != null) {
+			if (nFields > 0)
+				sb.append(",\n");
+
+			sb.append(offset + "\t\"REGION_TYPE\" : " + JsonUtilities.objectToJsonField(regionType));
+			nFields++;
+		}
+		
+		if (FlagNumOfChars) {
+			if (nFields > 0)
+				sb.append(",\n");
+
+			sb.append(offset + "\t\"NUM_OF_CHARS\" : " + JsonUtilities.objectToJsonField(numOfChars));
+			nFields++;
+		}
+		
+		if (FlagBeginIndex) {
+			if (nFields > 0)
+				sb.append(",\n");
+
+			sb.append(offset + "\t\"BEGIN_INDEX\" : " + JsonUtilities.objectToJsonField(beginIndex));
+			nFields++;
+		}
+		
+		if (FlagEndIndex) {
+			if (nFields > 0)
+				sb.append(",\n");
+
+			sb.append(offset + "\t\"END_INDEX\" : " + JsonUtilities.objectToJsonField(endIndex));
+			nFields++;
+		}
+		
+		
+		if (nFields > 0)
+			sb.append("\n");
+
+		sb.append(offset + "}");
+
+		return sb.toString();
+		
+	}	
+
 	
 }
