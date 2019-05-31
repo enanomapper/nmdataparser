5504d1ef08262c2ea0e06067e5a54330829dabba
n10705013 <jeliazkova.nina@gmail.com>
2019-05-31 18:05:58 +0300

    rewritten as junit test  and syntax error in json file fixed but the test is disfunctional , see the comment

--- enmexcelparser/src/test/java/net/enanomapper/parser/test/NMParserTest.java
+++ enmexcelparser/src/test/java/net/enanomapper/parser/test/NMParserTest.java
@@ -1,98 +1,71 @@
 package net.enanomapper.parser.test;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import net.enanomapper.parser.GenericExcelParser;
-
-import org.openscience.cdk.tools.LoggingTool;
+import org.junit.Test;
 
 import ambit2.base.data.SubstanceRecord;
-import ambit2.base.data.study.ProtocolApplication;
-import ambit2.base.relation.composition.CompositionRelation;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import net.enanomapper.parser.GenericExcelParser;
 
-public class NMParserTest extends TestCase
-{
-	public LoggingTool logger;
-	
-	public NMParserTest() throws Exception
-	{   
-		logger = new LoggingTool(this);
-		//initFileData();
+public class NMParserTest {
+	@Test
+	public void test_empty() throws Exception {
+		
 	}
+	// @Test
+	//Disabled - the xlsx file does not have sheet 3, which is refered by the json. 
 	
-	void initFileData() throws Exception
-	{
-		InputStream fin = null;
-		try {
+	public void test() throws Exception {
+		// this will close the inputstream automatically
+		try (InputStream fin = getClass().getClassLoader()
+				.getResourceAsStream("net/enanomapper/parser/testExcelParser/testfile1.xlsx")) {
 			boolean isXLSX = true;
-			System.out.println("isXLSX = " + isXLSX + "\n");
-
-			fin = getClass().getClassLoader()
-					.getResourceAsStream("net/enanomapper/parser/testExcelParser/testfile1.xlsx");
-			
-			
 			URL url = getClass().getClassLoader()
 					.getResource("net/enanomapper/parser/testExcelParser/testfile1-config.json");
-			GenericExcelParser parser = new GenericExcelParser(fin, new File(url.getFile()), isXLSX);
-			
-			
+			try (GenericExcelParser parser = new GenericExcelParser(fin, new File(url.getFile()), isXLSX)) {
 
-			//System.out.println(parser.getExcelParserConfigurator().toJSONString() + "\n");
-
-			int n = 0;
-			while (parser.hasNext()) 
-			{
-				SubstanceRecord r = parser.nextRecord();
-				n++;
-				System.out.println("Record #" + n);
-				/*
-				System.out.println(r.toJSON(null));
-				List<ProtocolApplication> paList = r.getMeasurements();
-
-				if (paList != null)
-					for (ProtocolApplication pa : paList)
-						System.out.println( "***Protocol application:\n"
-								+ pa.toString());
-
-				List<CompositionRelation> composition = r
-						.getRelatedStructures();
-				if (composition != null)
-					for (CompositionRelation relation : composition) {
-						// System.out.println(" ### Composition " +
-						// structureRecordToString(relation.getSecondStructure()));
-						System.out.println(
-								" ### Composition \n"
-										+ NMParserTestUtils.compositionRelationStructureToString(relation)); // both give
-																							// the same result
-						System.out.println(
-								" ### Properties: "
-										+ NMParserTestUtils.structureRecordProperties(relation
-												.getSecondStructure()));
-					}
-
-				 */	
+				// System.out.println(parser.getExcelParserConfigurator().toJSONString()
+				// + "\n");
+				
+				int n = 0;
+				while (parser.hasNext()) {
+					SubstanceRecord r = parser.nextRecord();
+					n++;
+					System.out.println("Record #" + n);
+					/*
+					 * System.out.println(r.toJSON(null));
+					 * List<ProtocolApplication> paList = r.getMeasurements();
+					 * 
+					 * if (paList != null) for (ProtocolApplication pa : paList)
+					 * System.out.println( "***Protocol application:\n" +
+					 * pa.toString());
+					 * 
+					 * List<CompositionRelation> composition = r
+					 * .getRelatedStructures(); if (composition != null) for
+					 * (CompositionRelation relation : composition) { //
+					 * System.out.println(" ### Composition " + //
+					 * structureRecordToString(relation.getSecondStructure()));
+					 * System.out.println( " ### Composition \n" +
+					 * NMParserTestUtils.compositionRelationStructureToString(
+					 * relation)); // both give // the same result
+					 * System.out.println( " ### Properties: " +
+					 * NMParserTestUtils.structureRecordProperties(relation
+					 * .getSecondStructure())); }
+					 * 
+					 */
+				}
+			} catch (Exception x) {
+				Logger.getAnonymousLogger().log(Level.SEVERE,x.getMessage());
+				throw x;
 			}
-		}	
-		finally {
-
-			if (fin != null)
-				fin.close();
+		} catch (Exception x1) {
+			Logger.getAnonymousLogger().log(Level.SEVERE,x1.getMessage());
+			throw x1;
 		}
 	}
-	
-	public static Test suite() {
-		return new TestSuite(NMParserTest.class);
-	}
-	
-	public void testTest1() throws Exception 
-	{
-		//TODO
-	}
+
 }
