42384c491c9b31f7edaae5294793dd4bdbf821a4
ntk73 <nick@uni-plovdiv.net>
2020-04-27 01:42:06 +0300

    Added helper functions objectArratToStringArray() and extractKeyword()

--- enmexcelparser/src/main/java/net/enanomapper/parser/SubstanceRecordMapLocation.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/SubstanceRecordMapLocation.java
@@ -1,5 +1,7 @@
 package net.enanomapper.parser;
 
+import java.util.List;
+
 import com.fasterxml.jackson.databind.JsonNode;
 
 import net.enanomapper.parser.json.JsonUtilities;
@@ -15,22 +17,63 @@ public class SubstanceRecordMapLocation
 		SubstanceRecordMapLocation srml = new SubstanceRecordMapLocation();
 
 		JsonUtilities jsonUtils = new JsonUtilities();
-		String strKeyword;
-
+		
 		// SUBSTANCE_NAME
 		if (node.path(KEYWORD.SUBSTANCE_NAME.name()).isMissingNode()) {
-			
+			conf.addError("In section SUBSTANCE_RECORD_MAP, keyword " + KEYWORD.SUBSTANCE_NAME.name() + " is missing");
 		}
 		else {
-			strKeyword = jsonUtils.extractStringKeyword(node, KEYWORD.SUBSTANCE_NAME.name(), false);
-			if (strKeyword == null)
-				conf.addError("In section SUBSTANCE_RECORD_MAP, keyword SUBSTANCE_NAME: " + jsonUtils.getError());
-			else {
-				srml.substanceNameVariable = strKeyword;
-			}
+			Object obj = extractKeyword(node, conf, KEYWORD.SUBSTANCE_NAME.name(), jsonUtils);
+			if (obj != null) {
+				if (obj instanceof String)
+					srml.substanceNameVariable = (String) obj;
+				else
+					srml.substanceNameArray = (String[]) obj;
+			}		
 		}
 		
 		return srml;
-	}	
+	}
+	
+	static Object extractKeyword(JsonNode node, ExcelParserConfigurator conf, String keyword, JsonUtilities jsonUtils)
+	{
+		if (node.path(keyword).isTextual()) {
+			String s = jsonUtils.extractStringKeyword(node, keyword, false);
+			if (s == null)
+			{	
+				conf.addError("In section SUBSTANCE_RECORD_MAP, keyword " + keyword + ": " + jsonUtils.getError());
+				return null;
+			}
+			return s;
+		}
+		else if (node.path(keyword).isArray()) {
+			Object obj = JsonUtilities.extractObject(node.path(keyword));
+			if (obj == null)
+			{	
+				conf.addError("In section SUBSTANCE_RECORD_MAP, keyword " + keyword + ": " + jsonUtils.getError());
+				return null;
+			}
+			String s[] = objectArrayToStringArray((Object[])obj);
+			if (s == null)
+				conf.addError("In section SUBSTANCE_RECORD_MAP, keyword " + keyword + ": is not correct array of strings!");
+			return s;
+		}else {
+			conf.addError("In section SUBSTANCE_RECORD_MAP, keyword " + keyword + ": must be TEXTUAL or ARRAY");
+			return null;
+		}	
+	}
+	
+	static String[] objectArrayToStringArray(Object obj[])
+	{
+		String s[] = new String[obj.length];
+		for (int i = 0; i < obj.length; i++)
+		{
+			if (obj[i] instanceof String)
+				s[i] = (String) obj[i];
+			else
+				return null;
+		}
+		return s;
+	}
 
 }
