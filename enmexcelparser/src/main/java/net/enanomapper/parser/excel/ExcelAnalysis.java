862b41531c364aba29a0f29db6d20a4659f4dd92
Nikolay Kochev <nick@uni-plovdiv.net>
2021-07-22 11:59:44 +0300

    Added new class ExcelAnalysis

---
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/ExcelAnalysis.java
@@ -0,0 +1,21 @@
+package net.enanomapper.parser.excel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcelAnalysis 
+{
+	public String targetPath = null;
+	public List<String> analysisResult = new ArrayList<String>();
+	
+	
+	public void init() {
+		analysisResult.clear();
+	}
+	
+	public void run() {
+		init();
+		//TODO
+	}
+	 
+}
