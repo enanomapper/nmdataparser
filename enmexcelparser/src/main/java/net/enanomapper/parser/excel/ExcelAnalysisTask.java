524729576cd02904fbe94a38e524ec6e1f0d0823
ntk73 <nick@uni-plovdiv.net>
2021-08-11 18:08:22 +0300

    Added class fields: type, params, targetFile

--- enmexcelparser/src/main/java/net/enanomapper/parser/excel/ExcelAnalysisTask.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/ExcelAnalysisTask.java
@@ -1,6 +1,16 @@
 package net.enanomapper.parser.excel;
 
+import java.io.File;
+
 public class ExcelAnalysisTask 
 {
+	public static enum TaskType {
+		COMPARE, COUNT, UNDEFINED
+	}
+	
+	public TaskType type = TaskType.UNDEFINED;
+	public Object params[] = null;
+	public File targetFile1 = null;
+	public File targetFile21 = null;	
 	
 }
