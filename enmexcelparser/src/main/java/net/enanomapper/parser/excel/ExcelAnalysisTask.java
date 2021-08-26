4de1f8347d9ec24e1249814e147bb05fd24390e6
ntk73 <nick@uni-plovdiv.net>
2021-08-26 16:38:15 +0300

    Added function fromString()

--- enmexcelparser/src/main/java/net/enanomapper/parser/excel/ExcelAnalysisTask.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/ExcelAnalysisTask.java
@@ -5,12 +5,21 @@ import java.io.File;
 public class ExcelAnalysisTask 
 {
 	public static enum TaskType {
-		COMPARE, COUNT, UNDEFINED
+		COMPARE_FILES, CHECK_VALUE, COUNT, UNDEFINED;
+		
+		public static TaskType fromString(String s) {
+			try {
+				TaskType type = TaskType.valueOf(s);
+				return (type);
+			} catch (Exception e) {
+				return TaskType.UNDEFINED;
+			}
+		}		
 	}
 	
 	public TaskType type = TaskType.UNDEFINED;
 	public Object params[] = null;
 	public File targetFile1 = null;
-	public File targetFile21 = null;	
+	public File targetFile2 = null;	
 	
 }
