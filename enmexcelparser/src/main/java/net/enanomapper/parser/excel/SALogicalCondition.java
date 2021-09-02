d7165e11805a028f83013796a1dd8c2d0d330ddc
ntk73 <nick@uni-plovdiv.net>
2021-09-02 16:05:01 +0300

    Added function parseFromString() in class SALogicalCondition

--- enmexcelparser/src/main/java/net/enanomapper/parser/excel/SALogicalCondition.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/SALogicalCondition.java
@@ -1,5 +1,8 @@
 package net.enanomapper.parser.excel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SALogicalCondition 
 {
 	public static enum LogicalConditionType {
@@ -26,9 +29,43 @@ public class SALogicalCondition
 		}		
 	}
 	
-	LogicalConditionType conditionType = LogicalConditionType.UNDEFINED;
-	TargetType targetType = TargetType.UNDEFINED;
+	public LogicalConditionType conditionType = LogicalConditionType.UNDEFINED;
+	public TargetType targetType = TargetType.UNDEFINED;
+	public String qualifier = null;
 	public Object params[] = null;
+	
+	
+	/*
+	 * Parsing an  SALogicalCondition from a string in the following format
+	 * <log.cond. type> <qualifier> <params> 
+	 */
+	public static SALogicalCondition parseFromString(String taskStr) throws Exception 
+	{
+		List<String> errors = new ArrayList<String>();
+		SALogicalCondition saLogCond = new SALogicalCondition();		
+		String tokens[] = taskStr.split(";");
+				
+		//SA Task type
+		if (tokens.length < 1)
+			errors.add("Missing task type token!");
+		else {	
+			saLogCond.conditionType = LogicalConditionType.fromString(tokens[0].trim());
+			if (saLogCond.conditionType == LogicalConditionType.UNDEFINED)
+				errors.add("Incorrect substance analysis task type: " + tokens[0]);
+		}
+		
+		//TODO
+
+		
+		if (!errors.isEmpty()) {
+			StringBuffer errBuffer = new StringBuffer();
+			for (String err: errors)
+				errBuffer.append(err + "\n");
+			throw new Exception (errBuffer.toString());
+		}		
+		
+		return saLogCond;
+	}
 	
 	
 	public boolean apply(Object target) {
