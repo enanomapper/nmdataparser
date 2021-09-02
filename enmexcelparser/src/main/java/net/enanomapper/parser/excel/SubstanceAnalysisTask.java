e97a66e6c90b008de77eda4ac18cf2652080e43a
ntk73 <nick@uni-plovdiv.net>
2021-09-02 15:44:44 +0300

    Added function parseFromString()

--- enmexcelparser/src/main/java/net/enanomapper/parser/excel/SubstanceAnalysisTask.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/SubstanceAnalysisTask.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ambit2.base.data.SubstanceRecord;
+import net.enanomapper.parser.excel.ExcelAnalysisTask.TaskType;
 
 public class SubstanceAnalysisTask 
 {
@@ -27,6 +28,37 @@ public class SubstanceAnalysisTask
 	public boolean flagVerboseResult = false;	
 	public List<String> analysisResult = new ArrayList<String>();
 	
+	/*
+	 * Parsing an  SubstanceAnalysisTask from a string in the following format
+	 * <task type>; <Logical conditions>; 
+	 */
+	public static SubstanceAnalysisTask parseFromString(String taskStr) throws Exception 
+	{
+		List<String> errors = new ArrayList<String>();
+		SubstanceAnalysisTask saTask = new SubstanceAnalysisTask();		
+		String tokens[] = taskStr.split(";");
+				
+		//SA Task type
+		if (tokens.length < 1)
+			errors.add("Missing task type token!");
+		else {	
+			saTask.type = SATaskType.fromString(tokens[0].trim());
+			if (saTask.type == SATaskType.UNDEFINED)
+				errors.add("Incorrect substance analysis task type: " + tokens[0]);
+		}
+		
+		//TODO
+
+		
+		if (!errors.isEmpty()) {
+			StringBuffer errBuffer = new StringBuffer();
+			for (String err: errors)
+				errBuffer.append(err + "\n");
+			throw new Exception (errBuffer.toString());
+		}		
+		
+		return saTask;
+	}
 	
 	public int run(List<SubstanceRecord> records) 
 	{
