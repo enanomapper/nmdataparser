9158230b5f7cd781b785c7327919e4e6addeda3e
ntk73 <nick@uni-plovdiv.net>
2021-09-02 15:19:12 +0300

    Added enum TargetType

--- enmexcelparser/src/main/java/net/enanomapper/parser/excel/SALogicalCondition.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/SALogicalCondition.java
@@ -3,8 +3,7 @@ package net.enanomapper.parser.excel;
 public class SALogicalCondition 
 {
 	public static enum LogicalConditionType {
-		VALUE, UNDEFINED;
-		
+		VALUE, NULL, UNDEFINED;
 		public static LogicalConditionType fromString(String s) {
 			try {
 				LogicalConditionType type = LogicalConditionType.valueOf(s);
@@ -15,5 +14,30 @@ public class SALogicalCondition
 		}		
 	}
 	
-	LogicalConditionType type = LogicalConditionType.UNDEFINED;
+	public static enum TargetType {
+		SUBSTANCE, PROTOCOL_PARAMETER, EFFECT, CONDITION, UNDEFINED;
+		public static TargetType fromString(String s) {
+			try {
+				TargetType type = TargetType.valueOf(s);
+				return (type);
+			} catch (Exception e) {
+				return TargetType.UNDEFINED;
+			}
+		}		
+	}
+	
+	LogicalConditionType conditionType = LogicalConditionType.UNDEFINED;
+	TargetType targetType = TargetType.UNDEFINED;
+	public Object params[] = null;
+	
+	
+	public boolean apply(Object target) {
+		//TODO
+		return false;
+	}
+	
+	public boolean applyForSubstance() {
+		//TODO
+		return false;
+	}
 }
