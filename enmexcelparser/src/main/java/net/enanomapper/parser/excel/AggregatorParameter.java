3e32bb912da970d88f8d3343cdc619e1689b57a5
ntk73 <nick@uni-plovdiv.net>
2022-03-06 21:14:33 +0200

    Added basic class fields and helper enums

--- enmexcelparser/src/main/java/net/enanomapper/parser/excel/AggregatorParameter.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/AggregatorParameter.java
@@ -5,6 +5,22 @@ import java.util.List;
 
 public class AggregatorParameter 
 {
+	public enum SubstanceElement {
+		CONDITION, PROTOCOL_PARAMETER, SUBSTANCE_NAME;
+	}
+	
+	public enum AggregationTarget {
+		BLOCK_AGGREGATION, BLOCK, SUBBLOCK, VALUE_GROUP;
+	}
+	
+	public AggregatorParameter(String name) {
+		this.name = name;
+	}
+	
 	public String name = null;
+	public SubstanceElement substanceElement = SubstanceElement.CONDITION;
+	public AggregationTarget aggregationTarget = AggregationTarget.VALUE_GROUP; 
+	public boolean isHorizontalOrientation = false;
+	public int blockAggregationLevel = 1;
 	public List<Object> values = new ArrayList<Object>();
 }
