9a236335630609fdcf57ade0a79a39b8822d6989
ntk73 <nick@uni-plovdiv.net>
2022-03-06 20:55:25 +0200

    Added new class AggregatorParameter

---
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/AggregatorParameter.java
@@ -0,0 +1,10 @@
+package net.enanomapper.parser.excel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AggregatorParameter 
+{
+	public String name = null;
+	public List<Object> values = new ArrayList<Object>();
+}
