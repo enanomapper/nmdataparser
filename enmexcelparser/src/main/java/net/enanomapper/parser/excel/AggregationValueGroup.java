1083f71f054a86be4e95049c33881876576c4a36
ntk73 <nick@uni-plovdiv.net>
2022-03-06 21:33:53 +0200

    Added class AggregatorValueGroup

---
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/AggregationValueGroup.java
@@ -0,0 +1,9 @@
+package net.enanomapper.parser.excel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AggregationValueGroup 
+{
+	public List<AggregatorParameter> parameters = new ArrayList<AggregatorParameter>();
+}
