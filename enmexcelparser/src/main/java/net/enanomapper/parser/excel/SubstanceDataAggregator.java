dc7eadea9512ca741f676de81d443b27b4421884
ntk73 <nick@uni-plovdiv.net>
2022-03-07 13:06:33 +0200

    Code reoganization. Added basic functions

--- enmexcelparser/src/main/java/net/enanomapper/parser/excel/SubstanceDataAggregator.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/SubstanceDataAggregator.java
@@ -1,5 +1,6 @@
 package net.enanomapper.parser.excel;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,9 +35,30 @@ public class SubstanceDataAggregator
 	public int columnSubblocks = 1;
 	public int subblockSizeRows = 1;
 	public int subblockSizeColumns = 1;	
+	
+	public List<AggregationValueGroup> valueGroups = new  ArrayList<AggregationValueGroup>();
+	public List<AggregatorParameter> agregatorParameters = new  ArrayList<AggregatorParameter>();
 	public Map<String, String> expressions = new HashMap<String, String>();
 	
-
+	public double dataMatrix[][] = null;
+	
+	
+	public void aggregate(IRawReader<IStructureRecord> substanceIterator)
+	{
+		//TODO
+	}
+	
+	public void aggregate(List<SubstanceRecord> substanceList)
+	{
+		//TODO
+	}
+	
+	public void simulateData()
+	{
+		//TODO
+	}
+	
+	
 	public void iterate(IRawReader<IStructureRecord> substanceIterator, IterationTask itTask)
 	{
 		if (substanceIterator == null)
@@ -44,37 +66,53 @@ public class SubstanceDataAggregator
 
 		while (substanceIterator.hasNext()) {
 			SubstanceRecord r = (SubstanceRecord)substanceIterator.nextRecord();
-			switch (itTask) {
-			case ANALYSE:
-				analyse(r);
-				break;
-			case AGGREGATE:
-				//TODO
-				break;
-			}
+			iterate (r, itTask);
+		}
+	}
+	
+	public void iterate(List<SubstanceRecord> substanceList, IterationTask itTask)
+	{		
+		for (SubstanceRecord r : substanceList) {
+			iterate (r, itTask);
 		}
 	}
 
-	public void analyse(SubstanceRecord rec) 
+	public void iterate(SubstanceRecord rec, IterationTask itTask) 
 	{
 		List<ProtocolApplication> paList = rec.getMeasurements();
 		for (ProtocolApplication pa : paList) 
 		{
-			analyse(pa);
+			iterate(pa, itTask);
 		}	
 	}
 
 
-	public void analyse(ProtocolApplication pa)
+	public void iterate(ProtocolApplication pa, IterationTask itTask)
 	{
 		List<EffectRecord> effects = pa.getEffects();
 		for (EffectRecord eff : effects)
 		{
-
+			iterate(eff, itTask);
 		}
 	}
 
-	public void analyse(EffectRecord eff)
+	public void iterate(EffectRecord eff, IterationTask itTask)
+	{
+		switch (itTask) {
+		case ANALYSE:
+			//TODO
+			break;
+		case AGGREGATE:
+			//TODO
+			break;
+		
+		case DATA_SIMULATION:
+			//TODO
+			break;
+		}
+	}
+	
+	public void exportDataMatrixToCSV(String fileName) throws Exception 
 	{
 		//TODO
 	}
