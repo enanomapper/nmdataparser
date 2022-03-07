84502bbd211fc0c3e39b28ea5569fe122a0ad22a
ntk73 <nick@uni-plovdiv.net>
2022-03-07 11:01:02 +0200

    Added basic fields in class AggregationValueGroup

--- enmexcelparser/src/main/java/net/enanomapper/parser/excel/AggregationValueGroup.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/excel/AggregationValueGroup.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class AggregationValueGroup 
 {
+	public String endpointName = null;
+	public String endpointType = null;
 	public List<AggregatorParameter> parameters = new ArrayList<AggregatorParameter>();
+	
+	//Position within a sub-block
+	public int startRow = 0;
+	public int endRow = 0;
+	public int rowSize = 0;
+	public int columnSize = 0;
+	
 }
