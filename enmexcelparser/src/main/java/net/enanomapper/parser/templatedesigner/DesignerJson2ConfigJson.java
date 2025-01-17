15af47f161be3aec474f0a8301792fc37895c8a3
Nikolay Kochev <nick@uni-plovdiv.net>
2025-01-17 14:09:21 +0200

    Added basic class constructor and function getConfigJson()

--- enmexcelparser/src/main/java/net/enanomapper/parser/templatedesigner/DesignerJson2ConfigJson.java
+++ enmexcelparser/src/main/java/net/enanomapper/parser/templatedesigner/DesignerJson2ConfigJson.java
@@ -1,5 +1,16 @@
 package net.enanomapper.parser.templatedesigner;
 
 public class DesignerJson2ConfigJson {
+	
+	private StringBuilder configJson = new StringBuilder();
+	private String designJsonFile = null;
+	
+	public DesignerJson2ConfigJson(String designJsonFile) {
+		this.designJsonFile = designJsonFile;
+	}
+	
+	public String getConfigJson() {
+		return configJson.toString();
+	}
 
 }
